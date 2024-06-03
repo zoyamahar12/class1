0c5005375f6dbe47d2c806e2e137fbfac5c821a0
zoyamahar12 <maharzoya68@gmail.com>
2024-06-02 21:35:34 -0700

    class 6

---
+++ index.js
@@ -0,0 +1,15 @@
+// let nam2 = " wasif";  
+// function greet (){
+//     console.log("hello students" );
+// }
+// greet()  //function calling 
+// greet()
+// greet()
+// greet() 
+// greet()
+
+
+
+let data =add( 22,22,33);
+let data2 =add(222,22,333);
+console.log(data,data2);
